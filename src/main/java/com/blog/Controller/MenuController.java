f99a3b0b7abc9865a35c28e00468ea95a4f66123
devmwpark <devmwpark@gmail.com>
2017-02-04 23:21:27 +0900

    변경 파일 삭제

--- src/main/java/com/blog/Controller/MenuController.java
+++
@@ -1,7 +0,0 @@
-package com.blog.Controller;
-
-/**
- * Created by learnrace on 2017. 2. 3..
- */
-public class MenuController {
-}
--- src/main/java/com/blog/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.repository;
-
-/**
- * Created by learnrace on 2017. 2. 3..
- */
-public class MenuRepository {
-}
