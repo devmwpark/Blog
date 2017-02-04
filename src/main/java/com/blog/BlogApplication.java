8c7f4dde07c4b656dba4d1373525c72090487b2b
devmwpark <devmwpark@gmail.com>
2017-02-05 01:56:06 +0900

    기본 설정 및 JSR310 설정 추가

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -1,12 +1,22 @@
 package com.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {BlogApplication.class, Jsr310JpaConverters.class} )
 public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelmapper() {
+		return new ModelMapper();
 	}
 }
