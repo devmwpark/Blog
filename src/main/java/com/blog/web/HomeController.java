168db22e3460edc7e20849730596cb40d6bfcc7f
devmwpark <devmwpark@gmail.com>
2017-02-05 18:42:41 +0900

    PutMapping Test

--- src/main/java/com/blog/web/HomeController.java
+++ src/main/java/com/blog/web/HomeController.java
@@ -1,16 +1,17 @@
 package com.blog.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author devmwpark[devmwpark@gmail.com]
  */
 
 @RestController
+@Slf4j
 @RequestMapping(value = "/home")
 public class HomeController {
 
@@ -18,4 +19,10 @@ public class HomeController {
     public ResponseEntity<String> home(){
         return new ResponseEntity<>("Hello World!!", HttpStatus.OK);
     }
+
+    @PutMapping(value = "/create")
+    public ResponseEntity putHome(@RequestBody  final String body){
+        return new ResponseEntity(body,HttpStatus.CREATED);
+    }
+
 }
