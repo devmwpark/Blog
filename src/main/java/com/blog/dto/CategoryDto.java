1155b3824cda994563c1de7cb8cf15cf5c49a2ea
devmwpark <devmwpark@gmail.com>
2017-02-06 23:56:19 +0900

    CategoryDto create

---
+++ src/main/java/com/blog/dto/CategoryDto.java
@@ -0,0 +1,32 @@
+package com.blog.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/**
+ * @author devmwpark[devmwpark@gmail.com]
+ */
+
+public class CategoryDto {
+
+    @Data
+    public static class Create {
+        @NotNull
+        private String name;
+        @NotNull
+        private Integer depth;
+        private Long parent;
+    }
+
+    @Data
+    public static class Read{
+        private Long id;
+        private String name;
+        private Integer depth;
+        private Long parent;
+        private LocalDateTime createTime;
+        private LocalDateTime updateTime;
+    }
+}
